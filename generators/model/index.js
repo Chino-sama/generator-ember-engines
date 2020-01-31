4ea8517d3cb8c8648751e72c10faa03ce8b649ee
Jessica Sánchez <michelle.sr16@gmail.com>
2020-01-31 13:58:58 -0600

    added subgenerator for models

---
+++ generators/model/index.js
@@ -0,0 +1,76 @@
+"use strict";
+const Generator = require("yeoman-generator");
+
+const camelCase = require("camelcase");
+
+module.exports = class extends Generator {
+	constructor(args, opts) {
+		super(args, opts);
+
+		this.argument("action", { type: String, required: true });
+		this.argument("addon", { type: String, required: true });
+		this.argument("model", { type: String, required: true });
+
+		this.option("no-root");
+	}
+
+	initializing() {
+		let generateActions = ["g", "generate"];
+		let deleteActions = ["d", "delete"];
+
+		if (generateActions.includes(this.options.action)) {
+			this.actionType = "generate";
+		} else if (deleteActions.includes(this.options.action)) {
+			this.actionType = "delete";
+		} else {
+			this.log.error(
+				"Unsupported action. Supported action types are generate and delete"
+			);
+			done();
+		}
+	}
+
+	writing() {
+		let addonName = camelCase(this.options.addon, { pascalCase: true });
+		let modelName = camelCase(this.options.model, { pascalCase: true });
+
+		if (this.actionType === "generate") {
+			if (!this.options["no-root"])
+				this.fs.copy(
+					this.templatePath("re-export.js"),
+					this.destinationPath(
+						`app/models/${this.options.addon}/${this.options.model}.js`
+					),
+					{
+						addonName: this.options.addon,
+						modelName: this.options.model
+					}
+				);
+			this.fs.copy(
+				this.templatePath("model.js"),
+				this.destinationPath(
+					`lib/api-${this.options.addon}/addon/models/${this.options.addon}/${this.options.model}.js`
+				),
+				{ addonName, modelName }
+			);
+			this.fs.copy(
+				this.templatePath("re-export.js"),
+				this.destinationPath(
+					`lib/api-${this.options.addon}/app/models/${this.options.addon}/${this.options.model}.js`
+				),
+				{ addonName: this.options.addon, modelName: this.options.model }
+			);
+		} else if (this.actionType === "delete") {
+			if (!this.options["no-root"])
+				this.fs.delete(
+					`app/models/${this.options.addon}/${this.options.model}.js`
+				);
+			this.fs.delete(
+				`lib/api-${this.options.addon}/addon/models/${this.options.addon}/${this.options.model}.js`
+			);
+			this.fs.delete(
+				`lib/api-${this.options.addon}/app/models/${this.options.addon}/${this.options.model}.js`
+			);
+		}
+	}
+};
---
+++ generators/model/templates/model.js
@@ -0,0 +1,5 @@
+import Model from '@ember-data/model';
+
+export default class <%= addonName %><%= modelName %>Model extends Model {
+
+}
